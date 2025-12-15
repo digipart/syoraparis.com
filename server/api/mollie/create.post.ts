e35ee7cf7cb56361f003fb78174da40e3e11b407
ojabar <104359715+ojabar@users.noreply.github.com>
2025-12-15 09:55:52 +0100

    Update create.post.ts

--- server/api/mollie/create.post.ts
+++ server/api/mollie/create.post.ts
@@ -1,49 +1,48 @@
 import { createMollieClient } from '@mollie/api-client';
 
 export default defineEventHandler(async (event) => {
-  try {
-    const {
-      method,
-      amount,
-      description,
-      orderId,
-      redirectUrl,
-      webhookUrl,
-      metadata,
-    } = await readBody(event);
+  const config = useRuntimeConfig();
+  console.log('config.public.mollieApiKey', config.public.mollieApiKey);
+  return 'hello world'
+  // try {
+  //   const {
+  //     method,
+  //     amount,
+  //     description,
+  //     orderId,
+  //     redirectUrl,
+  //     webhookUrl,
+  //     metadata,
+  //   } = await readBody(event);
 
-    if (!amount || !amount.currency || !amount.value) {
-      throw new Error('Invalid amount specified for payment.');
-    }
+  //   if (!amount || !amount.currency || !amount.value) {
+  //     throw new Error('Invalid amount specified for payment.');
+  //   }
 
-    
-    const config = useRuntimeConfig();
-    console.log('config.public.mollieApiKey', config.public.mollieApiKey);
-    
-    const mollie = createMollieClient({
-      apiKey: config.public.mollieApiKey,
-    });
+  //   const mollie = createMollieClient({
+  //     apiKey: config.public.mollieApiKey,
+  //   });
 
-    const payment = await mollie.payments.create({
-      amount: { currency: amount.currency, value: amount.value },
-      description: description,
-      method: method,
-      redirectUrl: redirectUrl,
-      webhookUrl: webhookUrl,
-      metadata,
-    });
+  //   const payment = await mollie.payments.create({
+  //     amount: { currency: amount.currency, value: amount.value },
+  //     description: description,
+  //     method: method,
+  //     redirectUrl: redirectUrl,
+  //     webhookUrl: webhookUrl,
+  //     metadata,
+  //   });
 
-    return {
-      paymentUrl: payment.getCheckoutUrl(),
-    };
-  } catch (error: any) {
-    console.error('Mollie payment creation failed:', error);
-    return createError({
-      statusCode: 500,
-      statusMessage: 'Server Error',
-      message:
-        error.message ||
-        'An unexpected error occurred while creating the payment.',
-    });
-  }
+  //   return {
+  //     paymentUrl: payment.getCheckoutUrl(),
+  //   };
+  // } catch (error: any) {
+  //   console.error('Mollie payment creation failed:', error);
+  //   return createError({
+  //     statusCode: 500,
+  //     statusMessage: 'Server Error',
+  //     message:
+  //       error.message ||
+  //       'An unexpected error occurred while creating the payment.',
+  //   });
+  // }
 });
