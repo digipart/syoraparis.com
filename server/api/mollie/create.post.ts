b7e605bf78eabbb8351370ba074fb9cd4d31ee79
ojabar <104359715+ojabar@users.noreply.github.com>
2025-12-15 10:05:19 +0100

    Update create.post.ts

--- server/api/mollie/create.post.ts
+++ server/api/mollie/create.post.ts
@@ -1,18 +1,25 @@
 import { createMollieClient } from '@mollie/api-client';
 
 export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const {
+    method,
+    amount,
+    description,
+    orderId,
+    redirectUrl,
+    webhookUrl,
+    metadata,
+  } = await readBody(event);
+  return {
+    amount: { currency: amount.currency, value: amount.value },
+    description: description,
+    method: method,
+    redirectUrl: redirectUrl,
+    webhookUrl: webhookUrl,
+    metadata,
+  };
   try {
-    const body = await readBody(event);
-    const {
-      method,
-      amount,
-      description,
-      orderId,
-      redirectUrl,
-      webhookUrl,
-      metadata,
-    } = await readBody(event);
-
     const config = useRuntimeConfig();
     const mollie = createMollieClient({
       apiKey: config.public.mollieApiKey,
@@ -31,6 +38,6 @@ export default defineEventHandler(async (event) => {
       paymentUrl: payment.getCheckoutUrl(),
     };
   } catch (error) {
-    return error
+    return error;
   }
 });
